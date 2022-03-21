ec5bd6907d90eecefb2b1da22dec8617b199de8f
King Oni <61396903+King-Oni@users.noreply.github.com>
2022-03-21 21:02:10 +0200

    Add files via upload

--- ARM.ino
+++ ARM.ino
@@ -6,36 +6,42 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
 
 void setup() {
-  pwm.begin();
-  pwm.setPWMFreq(FREQUENCY);
-  initate90();
+    pwm.begin();
+    pwm.setPWMFreq(FREQUENCY);
+    initate90();
 }
 
 void loop(){
-  //god help me
+    moveMotor(mAc,motorA);
+    moveMotor(mBc,motorB);
+    moveMotor(mCc,motorC);
+    moveMotor(mDc,motorD);
+    moveMotor(mEc,motorE);
+    moveMotor(mFc,motorF);
+    delay(50);
 }
 
 
 void moveMotor(int controlIn, int motorOut){
-  
-  int pulse_wide, pulse_width, potVal;
-  // Read values from potentiometer
-  potVal = analogRead(controlIn);
-  
-  // Convert to pulse width
-  pulse_wide = map(potVal, 0, 1023, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH);
-  pulse_width = int(float(pulse_wide) / 1000000 * FREQUENCY * 4096);
-  
-  //Control Motor
-  pwm.setPWM(motorOut, 0, pulse_width);
+    
+    int pulse_wide, pulse_width, potVal;
+    // Read values from potentiometer
+    potVal = analogRead(controlIn);
+    
+    // Convert to pulse width
+    pulse_wide = map(potVal, 0, 1023, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH);
+    pulse_width = int(float(pulse_wide) / 1000000 * FREQUENCY * 4096);
+    
+    //Control Motor
+    pwm.setPWM(motorOut, 0, pulse_width);
 }
 
 void initate90 (){
-  //Control Motor
-  pwm.setPWM(motorA, 0, init90v);
-  pwm.setPWM(motorB, 0, init90v);
-  pwm.setPWM(motorC, 0, init90v);
-  pwm.setPWM(motorD, 0, init90v);
-  pwm.setPWM(motorE, 0, init90v);
-  pwm.setPWM(motorF, 0, init90v);
+    //Control Motor
+    pwm.setPWM(motorA, 0, init90v);
+    pwm.setPWM(motorB, 0, init90v);
+    pwm.setPWM(motorC, 0, init90v);
+    pwm.setPWM(motorD, 0, init90v);
+    pwm.setPWM(motorE, 0, init90v);
+    pwm.setPWM(motorF, 0, init90v);
 }
